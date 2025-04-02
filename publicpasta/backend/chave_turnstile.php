7b8fdd4b4cf9eeef9e55dff0c4fe725014c1b3b0
newmax2025 <newmaxconsultas@gmail.com>
2025-04-02 12:36:29 -0300

    Update chave_turnstile.php

--- publicpasta/backend/chave_turnstile.php
+++ publicpasta/backend/chave_turnstile.php
@@ -2,7 +2,7 @@
 require 'config.php'; // Garante que a conexão com o banco de dados está disponível
 
 // Buscar a chave do Cloudflare na tabela 'config'
-$query = "SELECT valor FROM config WHERE chave = 'token_cloudflare' LIMIT 1";
+$query = "SELECT valor FROM config WHERE chave = 'token_cloudflarecom' LIMIT 1";
 $stmt = $conexao->prepare($query);
 $stmt->execute();
 $result = $stmt->get_result();
